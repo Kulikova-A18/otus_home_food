1909b4a95dcfc6de818a8b201c266cbd66d73518
Kulikova-A18 <USER@DESKTOP-INFFBBO>
2023-08-21 21:57:54 +0300

    21.08.2023

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -26,7 +26,7 @@ void main(List<String> args) {
   runApp(HomeScreen());
 }
 
-final List<_GroupControllers> _groupControllers = [];
+List<_GroupControllers> _groupControllers = [];
 List<Card> _name_imageCard = [];
 
 class _GroupControllers {
@@ -44,16 +44,57 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
+// РЕЦЕПТЫ
+Widget _listView_menu() {
+  final children = [
+    Column(
+      children: [
+        for (var i = 0; i < _groupControllers.length; i++) _name_imageCard[i]
+      ],
+    ),
+  ];
+
+  return SingleChildScrollView(
+      child: Container(
+    padding: const EdgeInsets.all(30.0),
+    child: Column(
+      children: children,
+    ),
+  ));
+}
+
+// ===========
+final sign_bool = false;
+
+TextEditingController loginController = TextEditingController();
+TextEditingController passwordController = TextEditingController();
+TextEditingController twopasswordController = TextEditingController();
+
 class _HomeScreenState extends State<HomeScreen> {
+  int a_len = 7;
+  void auth_void() {
+    print(sign_bool);
+    print(loginController.text);
+    print(passwordController.text);
+    print(twopasswordController.text);
+
+    loginController.clear();
+    passwordController.clear();
+    twopasswordController.clear();
+  }
+
   @override
   void initState() {
     createLists_menu(1, assets_1, "Пончики", "60 минут");
     createLists_menu(2, assets_2, "Домашние венские вафли", "10 минут");
-    createLists_menu(3, assets_3, "Нежное печенье с шоколадной крошкой", "60 минут");
+    createLists_menu(
+        3, assets_3, "Нежное печенье с шоколадной крошкой", "60 минут");
     createLists_menu(4, assets_4, "Домашний бургер", "35 минут");
     createLists_menu(5, assets_5, "Манты", "1 час 20 минут");
-    createLists_menu(6, assets_6, "Паста болоньезе классическая", "1 час 10 минут");
+    createLists_menu(
+        6, assets_6, "Паста болоньезе классическая", "1 час 10 минут");
     createLists_menu(7, assets_7, "Тонкие блины на молоке", "30 минут");
+
 
     super.initState();
   }
@@ -112,7 +153,11 @@ class _HomeScreenState extends State<HomeScreen> {
                                 const SizedBox(
                                   width: 20,
                                 ),
-                                Text(_titleTime, style: TextStyle(fontSize: 25, color: greenColor),),
+                                Text(
+                                  _titleTime,
+                                  style: TextStyle(
+                                      fontSize: 25, color: greenColor),
+                                ),
                               ],
                             ),
                           ),
@@ -147,31 +192,21 @@ class _HomeScreenState extends State<HomeScreen> {
   void createLists_menu(var _key, var _image, var _title, var _time) {
     var group = _GroupControllers();
 
-    var name_imageCard = _create_newCard(_key, _image, _title, _time);
+    var nameImagecard = _create_newCard(_key, _image, _title, _time);
 
     setState(() {
       // List<Card>
       _groupControllers.add(group);
-      _name_imageCard.add(name_imageCard);
+      _name_imageCard.add(nameImagecard);
     });
   }
 
-  Widget _listView() {
-    final children = [
-      Column(
-        children: [
-          for (var i = 0; i < _groupControllers.length; i++) _name_imageCard[i]
-        ],
-      ),
-    ];
+  int _selectedIndex = 0;
 
-    return SingleChildScrollView(
-        child: Container(
-      padding: const EdgeInsets.all(30.0),
-      child: Column(
-        children: children,
-      ),
-    ));
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
@@ -193,7 +228,12 @@ class _HomeScreenState extends State<HomeScreen> {
         return SafeArea(
           child: Scaffold(
             appBar: AppBar(
-              title: new Center(child: new Text("Рецепты", textAlign: TextAlign.center, style: unselectText,)),
+              title: Center(
+                  child: Text(
+                (_selectedIndex == 0) ? "Рецепты" : "",
+                textAlign: TextAlign.center,
+                style: unselectText,
+              )),
               iconTheme: IconThemeData(color: iconColor),
               elevation: 0.0,
               backgroundColor: backgroundColor,
@@ -207,10 +247,173 @@ class _HomeScreenState extends State<HomeScreen> {
                 //padding: const EdgeInsets.all(15.0),
                 child: Column(
                   children: [
-                    Expanded(child: _listView()),
+                    if(_selectedIndex == 0)
+                      Expanded(
+                          child: SingleChildScrollView(
+                              child: Container(
+                        padding: const EdgeInsets.all(30.0),
+                        child: Column(
+                          children: [
+                            Column(
+                              children: [
+                                for (var i = 0; i < _groupControllers.length; i++)
+                                   _name_imageCard[i]
+                                  //Text(i.toString())
+                              ],
+                            ),
+                          ],
+                        ),))),
+                      if(_selectedIndex == 1)
+                        (sign_bool == false)
+        ?
+        // --------- РЕГИСТРАЦИЯ ---------
+        Expanded(
+            child: SingleChildScrollView(
+                child: Container(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              children: [
+                Text(
+                  'HOME',
+                  style: unselectTextName,
+                ),
+                Text(
+                  'OTUS',
+                  style: unselectTextName,
+                ),
+                Text(
+                  'FOODS',
+                  style: unselectTextName,
+                ),SizedBox(height: 25,),
+                Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      child: TextField(
+                        controller: loginController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Логин',
+                        ),
+                      ),
+                    ),SizedBox(height: 25,),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                      child: TextField(
+                        obscureText: true,
+                        controller: passwordController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Пароль',
+                        ),
+                      ),
+                    ),SizedBox(height: 25,),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                      child: TextField(
+                        obscureText: true,
+                        controller: twopasswordController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Пароль еще раз',
+                        ),
+                      ),
+                    ),SizedBox(height: 25,),
+                    Container(
+                        height: 50,
+                        width: 150,
+                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: ElevatedButton(
+                          child: const Text('Вход'),
+                          onPressed: () {
+                            // print(loginController.text);
+                            // print(passwordController.text);
+                          },
+                        )),
+                  ],
+                ),
+              ],
+            ),
+          )))
+        :
+        // --------- ВХОД ---------
+        Expanded(
+            child: SingleChildScrollView(
+                child: Container(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              children: [
+                Text(
+                  'HOME',
+                  style: unselectTextName,
+                ),
+                Text(
+                  'OTUS',
+                  style: unselectTextName,
+                ),
+                Text(
+                  'FOODS',
+                  style: unselectTextName,
+                ),SizedBox(height: 25,),
+                Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      child: TextField(
+                        controller: loginController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Логин',
+                        ),
+                      ),
+                    ),SizedBox(height: 25,),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                      child: TextField(
+                        obscureText: true,
+                        controller: passwordController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Пароль',
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 25,),
+                    Container(
+                        height: 50,
+                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: ElevatedButton(
+                          child: const Text('Вход'),
+                          onPressed: () {
+                            // print(loginController.text);
+                            // print(passwordController.text);
+                          },
+                        )),
+                  ],
+                ),
+              ],
+            ),
+          
+  
+                    ))),
                   ],
                 ),
               ),
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.local_pizza),
+                  label: 'Рецепты',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.people),
+                  label: 'Вход',
+                ),
+              ],
+              currentIndex: _selectedIndex,
+              selectedItemColor: Colors.amber[800],
+              onTap: _onItemTapped,
             ),
           ),
         );
